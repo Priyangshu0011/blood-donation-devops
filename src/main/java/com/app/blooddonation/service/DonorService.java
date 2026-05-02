5f23640c0b83bc71ffc8a5727a36265308517005
Priyangshu Sett <priyangshusett2004@gmail.com>
2026-05-03 01:11:51 +0530

    Re-releasing Application v1.0

--- src/main/java/com/app/blooddonation/service/DonorService.java
+++ src/main/java/com/app/blooddonation/service/DonorService.java
@@ -3,7 +3,6 @@ package com.app.blooddonation.service;
 import com.app.blooddonation.model.Donor;
 import com.app.blooddonation.repository.DonorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,7 +13,10 @@ public class DonorService {
     @Autowired
     private DonorRepository donorRepository;
 
-    public Donor saveDonor(@NonNull Donor donor) {
+    public Donor saveDonor(Donor donor) {
+        if (donor == null) {
+            throw new IllegalArgumentException("Donor cannot be null");
+        }
         return donorRepository.save(donor);
     }
 
