fee2111e9d5ac610f994b18a35189e271c6adf26
Priyangshu Sett <priyangshusett2004@gmail.com>
2026-05-03 01:04:18 +0530

    Re-releasing Application

--- src/main/java/com/app/blooddonation/service/DonorService.java
+++ src/main/java/com/app/blooddonation/service/DonorService.java
@@ -3,6 +3,7 @@ package com.app.blooddonation.service;
 import com.app.blooddonation.model.Donor;
 import com.app.blooddonation.repository.DonorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,7 +14,7 @@ public class DonorService {
     @Autowired
     private DonorRepository donorRepository;
 
-    public Donor saveDonor(Donor donor) {
+    public Donor saveDonor(@NonNull Donor donor) {
         return donorRepository.save(donor);
     }
 
